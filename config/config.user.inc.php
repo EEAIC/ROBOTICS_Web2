8d805783db70a5b3ecde58aa235456f1c9cb4dbc
JK Lim <artherlim@gmail.com>
2019-02-23 13:41:03 +0900

    Update LIFO_skin

--- config/config.user.inc.php
+++ config/config.user.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('__DEBUG__', 1);
-define('__DEBUG_OUTPUT__', 1);
-define('__DEBUG_PROTECT__', 1);
+define('__DEBUG_OUTPUT__', 0);
+define('__DEBUG_PROTECT__', 0);
 define('__DEBUG_PROTECT_IP__', '127.0.0.1');
 define('__DEBUG_DB_OUTPUT__', 0);
 define('__LOG_SLOW_QUERY__', 0);

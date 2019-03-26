c6f92ba54deddeaf94db3defeeefb9e27987ce20
JK Lim <artherlim@gmail.com>
2019-03-26 21:17:28 +0900

    Add Chart exts

--- modules/editor/styles/tui.editor_default/style.js
+++ modules/editor/styles/tui.editor_default/style.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
     $('.xe_content').addClass("tui-editor-contents");
-  });
+
+    var editor = tui.Editor.factory({
+        viewer: true,
+        exts: ['chart']
+    })
+
+   
+    $('.xe_content').each(function(){
+        console.log(this.innerHTML);
+        var converted = editor.convertor.toHTMLWithCodeHightlight(this.innerHTML);
+        console.log(converted);
+        this.innerHTML = converted;
+    });
+    
+});
 

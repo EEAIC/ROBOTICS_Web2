9c631fcd73cbf399863fb2cfe371baf8e803cd6d
JK Lim <artherlim@gmail.com>
2019-03-27 13:01:54 +0900

    rename ToastUIEditor folder

--- modules/editor/styles/tui.editor_default/style.js
+++ modules/editor/styles/tui.editor_default/style.js
@@ -6,11 +6,8 @@ $(document).ready(function() {
         exts: ['chart', 'uml', 'table', 'youtube']
     })
 
-   
     $('.xe_content').each(function(){
-        console.log($(this).text());
         var converted = editor.convertor.toHTMLWithCodeHightlight($(this).text());
-        console.log(converted);
         $(this).html(converted);
     });
     
